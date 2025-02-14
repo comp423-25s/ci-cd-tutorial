ee8a22ccaff715b1c2b49f348c69b9a019746e72
Kris Jordan <kris@cs.unc.edu>
2025-02-14 13:49:13 +0000

    Add tests and GH Action

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 """FastAPI main entrypoint file."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from typing import Annotated
+from models import TimeZones
+from services import TimezoneService
 
 app = FastAPI()
 
-# TODO: Add routes here.
+
+TIMEZONES = [
+    "Africa/Johannesburg",
+    "America/New_York",
+]
+
+
+@app.get("/")
+def main(time_service: Annotated[TimezoneService, Depends()]) -> TimeZones:
+    return time_service.get_current_times_in_timezones(TIMEZONES)
